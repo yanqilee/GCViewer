d22e3eb02f271a70fdd4d4d286afceb8d676b358
chewiebug <jwu@gmx.ch>
2018-07-26 21:24:53 +0200

    add space before "]"

--- gcviewer-script.sh
+++ gcviewer-script.sh
@@ -6,7 +6,7 @@ echo "TRAVIS_JDK_VERSION = ${TRAVIS_JDK_VERSION}"
 
 # only deploy, if it is chewiebug/gcviewer#develop branch and openjdk8; for all other cases just verify
 # reason: avoid deploying multiple times to SourceForge
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "develop" ] && [ "${TRAVIS_JDK_VERSION}" = "openjdk8"]
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "develop" ] && [ "${TRAVIS_JDK_VERSION}" = "openjdk8" ]
 then
   echo build and deploy
   mvn clean deploy javadoc:javadoc -P sourceforge-release --settings ./target/travis/settings.xml
