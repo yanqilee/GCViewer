0ab0f2e57fc77926402e2878ad2d806a4fba7227
chewiebug <jwu@gmx.ch>
2018-07-26 21:09:16 +0200

    restrict deployment builds to "openjdk8"

--- gcviewer-script.sh
+++ gcviewer-script.sh
@@ -3,7 +3,10 @@ set -ev
 echo "TRAVIS_PULL_REQUEST = ${TRAVIS_PULL_REQUEST}"
 echo "TRAVIS_BRANCH = ${TRAVIS_BRANCH}"
 echo "TRAVIS_JDK_VERSION = ${TRAVIS_JDK_VERSION}"
-if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "develop" ]
+
+# only deploy, if it is chewiebug/gcviewer#develop branch and openjdk8; for all other cases just verify
+# reason: avoid deploying multiple times to SourceForge
+if [ "${TRAVIS_PULL_REQUEST}" = "false" ] && [ "${TRAVIS_BRANCH}" = "develop" ] && [ "${TRAVIS_JDK_VERSION}" = "openjdk8"]
 then
   echo build and deploy
   mvn clean deploy javadoc:javadoc -P sourceforge-release --settings ./target/travis/settings.xml
